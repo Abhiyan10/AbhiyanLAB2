3e757f467df4072f7c764821c37cd3442ad2b100
Abhiyan10 <abhiyanbhattarai@gmail.com>
2023-09-13 11:50:22 -0400

    final commit

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { View, Text, TextInput,Button, StyleSheet, Switch} from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+export default function HelloWorldApp() {
+  const [value, onChangeText] = 	React.useState('Enter your Name');  
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+ 
+  return (  
+  <View style={[styles.container, isEnabled && styles.dark]}>
+    <TextInput style={[styles.edit,isEnabled && styles.textWhite ]}
+	    onChangeText={text => onChangeText(text)}
+      clearTextOnFocus={true}
+      value={value}
+    />
+    <Text style={[styles.smalltext,isEnabled && styles.textWhite ] }>{value}</Text>
+   
+    <Button style={styles.button}
+       onPress={() => onChangeText('')}
+ 	      title="Clear" 
+       />
+         <Switch
+        trackColor={{false: '#767577', true: '#81b0ff'}}
+        thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
+        ios_backgroundColor="#3e3e3e"
+        onValueChange={toggleSwitch}
+        value={isEnabled}
+      />
+   </View> 
   );
 }
-
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+       flex: 1,
+       justifyContent: 'center',
+       alignItems: 'center'
+      },
+  edit: {
+       fontSize : 30
   },
+  smalltext: {
+    fontSize : 20
+},
+  dark:{
+    backgroundColor:"#000"
+  },
+  textWhite:{
+    color:"#fff"
+  }
 });
